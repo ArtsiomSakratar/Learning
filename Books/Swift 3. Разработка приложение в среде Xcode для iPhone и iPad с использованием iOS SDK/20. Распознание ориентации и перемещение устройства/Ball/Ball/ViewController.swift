e92f6de3f824086a82d059f2830c4f8f9ee0e7f0
Artsiom Sakratar <artsiom.sakratar@gmail.com>
2021-12-14 13:19:17 +0300

    add Ball app

---
+++ Books/Swift 3. Разработка приложение в среде Xcode для iPhone и iPad с использованием iOS SDK/20. Распознание ориентации и перемещение устройства/Ball/Ball/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Ball
+//
+//  Created by Artsiom Sakratar on 14/12/2021.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+	private static let updateInterval = 1.0/60.0
+	private let motionManager = CMMotionManager()
+	private let queue = OperationQueue()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+			// Do any additional setup after loading the view, typically from a nib.
+
+			//        motionManager.startDeviceMotionUpdates(to: queue) {
+			//                (motionData: CMDeviceMotion?, error: NSError?) -> Void in
+			//            let ballView = self.view as! BallView
+			//            ballView.acceleration = motionData!.gravity
+			//            DispatchQueue.main.async {
+			//                ballView.update()
+			//            }
+			//        }
+
+		motionManager.startDeviceMotionUpdates(to: queue, withHandler: {_,_ in
+			let ballView = self.view as! BallView
+				//            ballView.acceleration = motionData!.gravity
+			DispatchQueue.main.async {
+				ballView.update()
+			}
+
+		})
+
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+			// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
